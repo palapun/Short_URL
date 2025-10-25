5f7bfce911b49b9b928568d6875888890ef20ef0
Puno <punyawat5114@gmail.com>
2025-10-26 01:01:06 +0700

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
 // Development API URL - เปลี่ยนเป็น production URL หลังจาก deploy
 const API_BASE_URL = 'https://short-url-zxhk.onrender.com/api';
+
 function checkAuth() {
     const token = localStorage.getItem('userToken');
     if (!token) {
